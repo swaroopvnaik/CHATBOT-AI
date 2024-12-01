c0a1384e617eb88edd3a2d061a68a7a3fec849e1
Swaroop . V . Naik <125003490+swaroopvnaik@users.noreply.github.com>
2024-12-01 06:30:38 +0000

    doing

--- script.js
+++
@@ -1,9 +0,0 @@
-let prompt = document.querySelector("#prompt");
-
-
-prompt.addEventListener("keydown" , (e) =>{
-    if(e.target.key=="Enter"){
-        console.log(prompt.value);
-    }
-    console.log(e);
-});
